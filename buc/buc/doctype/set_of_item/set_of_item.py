396c9689636a72a44b1a20521b8c1a44c5610d0d
newtratip <tharathipc@ecosoft.co.th>
2025-05-31 01:02:15 +0700

    Allow set both serial and batch no in same line

--- buc/buc/doctype/set_of_item/set_of_item.py
+++ buc/buc/doctype/set_of_item/set_of_item.py
@@ -10,10 +10,10 @@ class SetofItem(Document):
 	def validate(self):
 		# Validate serial no / batch no
 		for item in self.items:
-			if item.serial_no and item.batch_no:
-				frappe.throw(
-					"Cannot set both serial no and batch no for an item %s." % (
-						item.item_code))
+			# if item.serial_no and item.batch_no:
+			# 	frappe.throw(
+			# 		"Cannot set both serial no and batch no for an item %s." % (
+			# 			item.item_code))
 			if not item.serial_no and not item.batch_no:
 				frappe.throw("Either serial no or batch no must be set for an item %s." % (
 					item.item_code))

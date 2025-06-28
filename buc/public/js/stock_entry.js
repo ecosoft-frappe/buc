e966bdbde27135b2e4a033901c716aed1ebad4e1
newtratip <tharathipc@ecosoft.co.th>
2025-06-28 22:12:42 +0700

    [IMP] cost center, department, division, bu in stock entry

--- buc/public/js/stock_entry.js
+++ buc/public/js/stock_entry.js
@@ -1,3 +1,96 @@
+frappe.ui.form.on("Stock Entry", {
+    onload: function(frm) {
+        if (frm.is_new() && !frm.doc.amended_from && !frm.doc.__onload) {
+            frappe.db.get_value("Employee", {"user_id": frappe.session.user}, ["cost_center", "department", "division", "business_unit"], (r) => {
+                frappe.db.get_value("Company", frm.doc.company, "cost_center", (k) => {
+                    let cost_center = r.cost_center
+                    let department = r.department
+                    let division = r.division
+                    let business_unit = r.business_unit
+                    if (cost_center === null || cost_center === undefined || cost_center === "") {
+                        cost_center = k.cost_center
+                    }
+
+                    frm.doc.cost_center = cost_center
+                    frm.doc.department = department
+                    frm.doc.division = division
+                    frm.doc.business_unit = business_unit
+
+                    setTimeout(() => {
+                        $.each(frm.doc.items, function(i, d){
+                            d.cost_center = cost_center,
+                            d.department = department,
+                            d.division = division,
+                            d.business_unit = business_unit
+                        });
+                    }, 5000);
+                })
+            })
+        }
+    },
+    refresh: function (frm) {
+        // Filter Division, Business Unit
+        frm.set_query("division", function() {
+            return {
+                filters: [
+                    ["department", "=", frm.doc.department]
+                ]
+            };
+        });
+        frm.set_query("business_unit", function() {
+            return {
+                filters: [
+                    ["division", "=", frm.doc.division]
+                ]
+            };
+        });
+        frm.fields_dict["items"].grid.get_field("division").get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                filters: [
+                    ["department", "=", row.department],
+                ]
+            };
+        };
+        frm.fields_dict["items"].grid.get_field("business_unit").get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                filters: [
+                    ["division", "=", row.division],
+                ]
+            };
+        };
+    },
+    cost_center: function (frm) {
+        setTimeout(() => {
+            $.each(frm.doc.items, function(i, d){
+                d.cost_center = frm.doc.cost_center
+            });
+        }, 1000);
+    },
+	department: function (frm) {
+        setTimeout(() => {
+            $.each(frm.doc.items, function(i, d){
+                d.department = frm.doc.department
+            });
+        }, 1000);
+	},
+	division: function (frm) {
+        setTimeout(() => {
+            $.each(frm.doc.items, function(i, d){
+                d.division = frm.doc.division
+            });
+        }, 1000);
+	},
+	business_unit: function (frm) {
+        setTimeout(() => {
+            $.each(frm.doc.items, function(i, d){
+                d.business_unit = frm.doc.business_unit
+            });
+        }, 1000);
+	},
+});
+
 frappe.ui.form.on("Stock Entry", "custom_get_serial__batch_no_from_set_of_item", function(frm) {
 
     if (!frm.doc.custom_set_of_item) {
@@ -65,4 +158,16 @@ frappe.ui.form.on("Stock Entry", "custom_get_serial__batch_no_from_set_of_item",
 
         frappe.msgprint(__("Serial / Batch No. updated successfully."));
     })
+});
+
+frappe.ui.form.on("Stock Entry Detail", {
+    items_add: function (frm, cdt, cdn) {
+        setTimeout(() => {
+            row = locals[cdt][cdn]
+            row.cost_center = frm.doc.cost_center
+            row.department = frm.doc.department
+            row.division = frm.doc.division
+            row.business_unit = frm.doc.business_unit
+        }, 1000)
+    },
 });
